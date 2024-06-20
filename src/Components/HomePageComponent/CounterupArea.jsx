2a31752abf5bd5bf66b7b918bf0827e93f8f355f
Hakan Yaman <hakan.yaman@d-teknoloji.com.tr>
2024-06-20 15:38:41 +0300

    update CounterupArea

--- src/Components/HomePageComponent/CounterupArea.jsx
+++ src/Components/HomePageComponent/CounterupArea.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
 
 const CounterupArea = () => {
+    const [ref, inView] = useInView({
+        threshold: 0.1,
+        triggerOnce: true
+    });
+
     return (
-        <div className="counterup-area" style={{backgroundImage: "url(counter_bg.jpg)"}}>
+        <div ref={ref} className="counterup-area" style={{backgroundImage: "url(counter_bg.jpg)"}}>
             <div className="bg-counterup"></div>
             <div className="container">
                 <div className="row">
                     <div className="col-md-3 col-sm-6 counter-border">
                         <div className="counter-item">
                             <h2 className="counter">
-                                <CountUp start={0} end={150} duration={2.75} />
+                                {inView && <CountUp start={0} end={150} duration={2.75} />}
                             </h2>
                             <h4>Working Persons</h4>
                         </div>
@@ -18,7 +24,7 @@ const CounterupArea = () => {
                     <div className="col-md-3 col-sm-6 counter-border">
                         <div className="counter-item">
                             <h2 className="counter">
-                                <CountUp start={0} end={1200} duration={2.75} />
+                                {inView && <CountUp start={0} end={1200} duration={2.75} />}
                             </h2>
                             <h4>Projects Completed</h4>
                         </div>
@@ -26,7 +32,7 @@ const CounterupArea = () => {
                     <div className="col-md-3 col-sm-6 counter-border">
                         <div className="counter-item">
                             <h2 className="counter">
-                                <CountUp start={0} end={800} duration={2.75} />
+                                {inView && <CountUp start={0} end={800} duration={2.75} />}
                             </h2>
                             <h4>Happy Clients</h4>
                         </div>
@@ -34,7 +40,7 @@ const CounterupArea = () => {
                     <div className="col-md-3 col-sm-6 counter-border">
                         <div className="counter-item achieved">
                             <h2 className="counter">
-                                <CountUp start={0} end={1200} duration={2.75} />
+                                {inView && <CountUp start={0} end={1200} duration={2.75} />}
                             </h2>
                             <h4>Awards Achieved</h4>
                         </div>
