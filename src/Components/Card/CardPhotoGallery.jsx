47bf0252502ef6ff7e1ba83a41153a568c1275dc
Hakan Yaman <hakan.yaman@d-teknoloji.com.tr>
2024-06-20 13:13:46 +0300

    update PHOTO GALLERY

--- src/Components/Card/CardPhotoGallery.jsx
+++ src/Components/Card/CardPhotoGallery.jsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { FaSearchPlus } from "react-icons/fa";
 import { useMediaQuery } from 'react-responsive';
+import Lightbox from "yet-another-react-lightbox";
+import Download from "yet-another-react-lightbox/plugins/download";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Captions from "yet-another-react-lightbox/plugins/captions";
 
-const CardPhotoGallery = ({data,setOpen}) => {
-
+const CardPhotoGallery = ({data}) => {
+    const [open, setOpen] = useState(false);
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
@@ -25,6 +29,12 @@ const CardPhotoGallery = ({data,setOpen}) => {
 
     return (
         <>
+            <Lightbox
+                open={open}
+                close={() => setOpen(false)}
+                slides={[{ src: data.url,  title: data.url.split('.')[0], downloadUrl: data.url, }]}
+                plugins={[Download, Fullscreen,Captions]}
+            />
             <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 ">
                 <div className="caption-item" onClick={handleCaptionClick}>
                     <div className="caption-photo">
--- src/Components/HomePageComponent/PhotoGallery.jsx
+++ src/Components/HomePageComponent/PhotoGallery.jsx
@@ -1,13 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import CardPhotoGallery from "@/Components/Card/CardPhotoGallery";
-import Lightbox from "yet-another-react-lightbox";
-import Download from "yet-another-react-lightbox/plugins/download";
-import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
-import Captions from "yet-another-react-lightbox/plugins/captions";
+
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 const PhotoGallery = () => {
-    const [open, setOpen] = useState(false);
     const photoJson = [
         {
             url:'photo-1.jpg',
@@ -42,11 +38,6 @@ const PhotoGallery = () => {
             name:'Photo 8'
         },
     ]
-    const slides = photoJson.map(photo => ({
-        src: photo.url,
-        downloadUrl: photo.url,
-        title: photo.url.split('.')[0]
-    }));
    return  <>
        <div className="container pb-5">
            <div className="headline">
@@ -54,14 +45,8 @@ const PhotoGallery = () => {
                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit Deserunt libero voluptate</p>
            </div>
         <div className="row">
-            <Lightbox
-                open={open}
-                close={() => setOpen(false)}
-                slides={slides}
-                plugins={[Download, Fullscreen,Captions]}
-            />
             {photoJson.map((photo,index) => {
-                return <CardPhotoGallery key={index} data={photo} setOpen={setOpen}/>
+                return <CardPhotoGallery key={index} data={photo}/>
             })}
         </div>
        </div>
